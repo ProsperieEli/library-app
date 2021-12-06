7c145e2575e014b72684309e5d2d75577072a82b
ProsperieEli <ElijahProsperie@gmail.com>
2021-12-06 14:17:52 -0500

    Saving work

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom'
+import { Header } from 'react'
 import BookList from './views/Books/BookList'
 import BookDetail from './views/Books/BookDetail'
+import Home from './views/Books/Home'
 import './App.css'
 
 function App() {
   // TODO: Add routes to books & views
   return (
-    <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
-    </main>
+    <Router>
+      <header>
+        Enter the Library.
+        <NavLink to="/" exact>
+          Home
+        </NavLink>
+        <NavLink to="BookList" exact>
+          List
+        </NavLink>
+        <NavLink to="BookDetail" exact>
+          Detail
+        </NavLink>
+      </header>
+      <Switch>
+        <Route path="/library" component={BookList} />
+        <Route path="/library/:bookid" component={BookDetail} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
   )
 }
 
